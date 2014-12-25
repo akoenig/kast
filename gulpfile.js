be3abeb96f86963ba9343c308d61aa731c3b0aca
André König <andre.koenig@posteo.de>
2014-12-25 17:29:06 +0100

    Initial revision. Project structure.

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/*
+ * meeting
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+var path = require('path');
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sequence = require('run-sequence');
+
+var paths = {};
+
+paths.sources = [path.join(__dirname, 'index.js')];
+
+gulp.task('lint', function () {
+    return gulp.src(paths.sources)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default', function (callback) {
+    return sequence('lint', callback);
+})
---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * meeting
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
