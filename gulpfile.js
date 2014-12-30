182e77e85629a8121b160ddbbcbface4c2112ee1
André König <andre.koenig@posteo.de>
2014-12-30 19:19:10 +0100

    Written specs for testing the front-facing API.

--- gulpfile.js
+++ gulpfile.js
@@ -18,14 +18,23 @@ var path = require('path');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var mocha = require('gulp-mocha');
 var sequence = require('run-sequence');
 
 var paths = {};
 
 paths.sources = [
     path.join(__dirname, '*.js'),
-    path.join(__dirname, 'lib', '**', '*.js')
+    path.join(__dirname, 'lib', '**', '*.js'),
+    path.join(__dirname, 'specs', '**', '*.spec.js')
 ];
+
+paths.specs = [path.join(__dirname, 'specs', '**', '*.spec.js')];
+
+gulp.task('specs', function specs () {
+    return gulp.src(paths.specs, {read: false})
+        .pipe(mocha({reporter: 'nyan'}));
+});
 
 gulp.task('lint', function lint () {
     return gulp.src(paths.sources)
@@ -40,5 +49,5 @@ gulp.task('checkstyle', function checkstyle () {
 });
 
 gulp.task('default', function defaultTask (callback) {
-    return sequence('lint', 'checkstyle', callback);
+    return sequence('lint', 'checkstyle', 'specs', callback);
 });
---
+++ specs/api.spec.js
@@ -0,0 +1,100 @@
+/*
+ * kast
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+var expect = require('expect.js');
+
+var kast = require('../');
+
+describe('The front-facing API', function suite () {
+
+    it('should have a `listen` method with proper argument checks', function test (done) {
+        var app = kast();
+
+        expect(app.listen).not.to.be(undefined);
+        expect(typeof app.listen).to.be('function');
+
+        try {
+            app.listen();
+        } catch (e) {
+            expect(e).not.to.be(undefined);
+        }
+
+        done();
+    });
+
+    it('should have a `command` method with proper argument checks', function test (done) {
+        var app = kast();
+
+        expect(app.command).not.to.be(undefined);
+        expect(typeof app.command).to.be('function');
+
+        try {
+            app.command();
+        } catch (e) {
+            expect(e).not.to.be(undefined);
+        }
+
+        try {
+            app.command('foo');
+        } catch (e) {
+            expect(e).not.to.be(undefined);
+        }
+
+        done();
+    });
+
+    it('should have a `broadcast` method with proper argument checks', function test (done) {
+        var success = true;
+
+        expect(kast.broadcast).not.to.be(undefined);
+        expect(typeof kast.broadcast).to.be('function');
+
+        try {
+            kast.broadcast();
+        } catch (e) {
+            expect(e).not.to.be(undefined);
+        }
+
+        //
+        // Missing required params
+        //
+        try {
+            kast.broadcast({});
+        } catch (e) {
+            expect(e).not.to.be(undefined);
+        }
+
+        try {
+            kast.broadcast({
+                port: 5000
+            });
+        } catch (e) {
+            expect(e).not.to.be(undefined);
+        }
+
+        try {
+            kast.broadcast({
+                port: 5000,
+                command: 'foo'
+            }, function onResponse () {});
+        } catch (e) {
+            success = false;
+        }
+
+        expect(success).to.be(true);
+
+        done();
+    });
+});
